6e0a66e7dbeec1aac35ede325e93b4a1db780950
Hieu Le <letrunghieu.cse09@gmail.com>
2015-09-23 14:47:34 +0700

    Less mixer with configurations

--- src/mixers/less.js
+++ src/mixers/less.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var _if = require('gulp-if');
+var _sourcemaps = require('gulp-sourcemaps');
+var _autoprefixer = require('gulp-autoprefixer');
+var _minifyCss = require('gulp-minify-css');
 
 var LessMixer = function (cocktail) {
     this.getOutputExt = function () {
@@ -7,8 +11,16 @@ var LessMixer = function (cocktail) {
     };
     
     this.mix = function (input, output) {
+        
+        var config = cocktail.config;
+        var options = cocktail.config.css.sass.pluginOptions;
+
         gulp.src(input)
-        .pipe(less())
+        .pipe(_if(!config.production && config.sourcemaps, _sourcemaps.init()))
+        .pipe(less(options))
+        .pipe(_if(config.css.autoprefix.enabled, _autoprefixer(config.css.autoprefix.options)))
+        .pipe(_if(config.production, _minifyCss()))
+        .pipe(_if(!config.production && config.sourcemaps, _sourcemaps.write('.')))
         .pipe(gulp.dest(output))
     }
     
