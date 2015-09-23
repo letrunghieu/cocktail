bc224c66b3bab251c3f49c9176b92ab5c658dec4
Hieu Le <letrunghieu.cse09@gmail.com>
2015-09-23 15:14:20 +0700

    Sprocket mixer with configurations

--- src/mixers/sprocket.js
+++ src/mixers/sprocket.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp');
 var include = require('gulp-include');
 var path = require('path');
+var _if = require('gulp-if');
+var _sourcemaps = require('gulp-sourcemaps');
+var _autoprefixer = require('gulp-autoprefixer');
+var _minifyCss = require('gulp-minify-css');
+var _uglify = require('gulp-uglify');
 
 var SprocketMixer = function (cocktail) {
     this.getOutputExt = function () {
@@ -8,12 +13,21 @@ var SprocketMixer = function (cocktail) {
     };
     
     this.mix = function (input, output) {
+        
+        var config = cocktail.config;
+        
         var ext = path.extname(input).substr(1);
+        
         gulp.src(input)
+        .pipe(_if(!config.production && config.sourcemaps, _sourcemaps.init()))
         .pipe(include())
+        .pipe(_if(ext === 'css' && config.css.autoprefix.enabled, _autoprefixer(config.css.autoprefix.options)))
+        .pipe(_if(ext === 'css' && config.production, _minifyCss()))
+        .pipe(_if(ext === 'js' && config.production, _uglify()))
+        .pipe(_if(!config.production && config.sourcemaps, _sourcemaps.write('.')))
         .pipe(gulp.dest(output));
     }
-
+    
     this.name = 'sprocket';
 }
 
--- test/assets/resources/js/app.js
+++ test/assets/resources/js/app.js
@@ -1,7 +1,54 @@
 /*=require _include.js  */
 //=include _include.js
 
-var x = 5;
-var y = 6;
-var z = x + y;
-document.getElementById("demo").innerHTML = z;
+(function() {
+  var cubes, list, math, num, number, opposite, race, square,
+    slice = [].slice;
+
+  alert("Hello CoffeeScript!");
+
+  number = 42;
+
+  opposite = true;
+
+  if (opposite) {
+    number = -42;
+  }
+
+  square = function(x) {
+    return x * x;
+  };
+
+  list = [1, 2, 3, 4, 5];
+
+  math = {
+    root: Math.sqrt,
+    square: square,
+    cube: function(x) {
+      return x * square(x);
+    }
+  };
+
+  race = function() {
+    var runners, winner;
+    winner = arguments[0], runners = 2 <= arguments.length ? slice.call(arguments, 1) : [];
+    return print(winner, runners);
+  };
+
+  if (typeof elvis !== "undefined" && elvis !== null) {
+    alert("I knew it!");
+  }
+
+  cubes = (function() {
+    var i, len, results;
+    results = [];
+    for (i = 0, len = list.length; i < len; i++) {
+      num = list[i];
+      results.push(math.cube(num));
+    }
+    return results;
+  })();
+
+}).call(this);
+
+//# sourceMappingURL=app.js.map
