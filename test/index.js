793189d88f54ef74d918d2ac644100f483e1a627
Hieu Le <letrunghieu.cse09@gmail.com>
2015-09-23 12:26:35 +0700

    Switch to BDD

--- test/index.js
+++ test/index.js
@@ -7,19 +7,23 @@ describe('Cocktail', function () {
     describe('#getOutputFile(ourceDir, file, buildDir)', function () {
         var cocktail = require('./../src/index');
         
-        it('should return correct destination file', function () {
-            assert.equal(path.resolve('/out/bar.png'), cocktail.getOutputFile('/in', '/in/bar.png', '/out'));
-            assert.equal(path.resolve('/out/foo/bar.png'), cocktail.getOutputFile('/in', '/in/foo/bar.png', '/out'));
+        given(
+            ['/in/bar.png', '/out/bar.png'],
+            ['/in/foo/bar.png', '/out/foo/bar.png'],
             // sass
-            assert.equal(path.resolve('/out/bar.css'), cocktail.getOutputFile('/in', '/in/bar.sass', '/out'));
-            assert.equal(path.resolve('/out/bar.css'), cocktail.getOutputFile('/in', '/in/bar.scss', '/out'));
+            ['/in/bar.sass', '/out/bar.css'],
+            ['/in/bar.scss', '/out/bar.css'],
             // less
-            assert.equal(path.resolve('/out/bar.css'), cocktail.getOutputFile('/in', '/in/bar.less', '/out'));
+            ['/in/bar.less', '/out/bar.css'],
             // coffee
-            assert.equal(path.resolve('/out/bar.js'), cocktail.getOutputFile('/in', '/in/bar.coffee', '/out'));
+            ['/in/bar.coffee', '/out/bar.js'],
             // sprocket
-            assert.equal(path.resolve('/out/bar.css'), cocktail.getOutputFile('/in', '/in/bar.css', '/out'));
-            assert.equal(path.resolve('/out/bar.js'), cocktail.getOutputFile('/in', '/in/bar.js', '/out'));
+            ['/in/bar.css', '/out/bar.css'],
+            ['/in/bar.js', '/out/bar.js']
+
+        ).it('should return correct destination file', function (file, result) {
+            expect(cocktail.getOutputFile('/in', file, '/out'))
+            .to.equals(path.resolve(result));
         });
     });
     
