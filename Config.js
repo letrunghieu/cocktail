3ecc77c86185d9c23d839f9cf5cb46bc78e1f1d8
Hieu Le <letrunghieu.cse09@gmail.com>
2015-09-23 10:10:27 +0700

    Update tabs to spaces

---
+++ Config.js
@@ -0,0 +1,5 @@
+var config = {
+	
+};
+
+module.export = config;
--- src/mixers/coffee.js
+++ src/mixers/coffee.js
@@ -1,19 +1,19 @@
 var fs = require('fs');
 
 var CoffeeMixer = function (cocktail) {
-	this.getOutputExt = function () {
-		return '.js';
-	};
-
-	this.mix = function (input, output) {
-		console.log(input, output);
-	}
+    this.getOutputExt = function () {
+        return '.js';
+    };
+    
+    this.mix = function (input, output) {
+        console.log(input, output);
+    }
 }
 
 module.exports = function (cocktail) {
-	var mixer = new CoffeeMixer(cocktail);
-
-	cocktail.registerMixer('.coffee', mixer);
-
-	return mixer;
+    var mixer = new CoffeeMixer(cocktail);
+    
+    cocktail.registerMixer('.coffee', mixer);
+    
+    return mixer;
 }
